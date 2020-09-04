80c12c57fb21d256fde0c5f1b0ea4d12123b3f22
yangfan <34465021+jwfl724168@users.noreply.github.com>
2020-09-04 23:25:26 +0800

    添加 0020 事件绑定 demo

---
+++ pages/0022/0022.js
@@ -0,0 +1,79 @@
+// pages/0022/0022.js
+Page({
+
+
+    _test3: ()=> {
+        console.log("_test3 接收到事件");
+    },
+
+    _test2: ()=> {
+        console.log("_test2 接收到事件");
+    },
+
+    _test1: ()=> {
+        console.log("_test1 接收到事件");
+    },
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
